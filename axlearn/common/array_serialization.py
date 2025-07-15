8fb1387f0a2779f830055ee1136a55b00f9ca5b0
Samuel Andersen <samuel@andersentech.net>
2025-07-15 20:18:50 +0000

    fix additional ts_impl refs

--- axlearn/common/array_serialization.py
+++ axlearn/common/array_serialization.py
@@ -417,7 +417,7 @@ async def _async_deserialize(
     async def cb(index: array.Index, device: jax.Device):
         requested_domain = ts.IndexTransform(input_shape=shape)[index].domain
         restricted_domain = t.domain.intersect(requested_domain)
-        requested_bytes = serialization.estimate_read_memory_footprint(t, restricted_domain)
+        requested_bytes = serialization.ts_impl.estimate_read_memory_footprint(t, restricted_domain)
         # Limit the bytes read for every shard.
         await byte_limiter.wait_for_bytes(requested_bytes)
         read_ts = t[restricted_domain]
@@ -560,7 +560,7 @@ class GlobalAsyncCheckpointManager(serialization.GlobalAsyncCheckpointManager):
         # pylint: disable-next=redefined-outer-name
         async def _run_serializer():
             future_writer = jax.tree.map(
-                serialization.async_serialize, arrays, tensorstore_specs, commit_futures
+                serialization.ts_impl.async_serialize, arrays, tensorstore_specs, commit_futures
             )
             return await asyncio.gather(*future_writer)
 
